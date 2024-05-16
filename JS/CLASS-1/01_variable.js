e22e5856e170236ee48a8b4104b73ed5532acad3
Swadhin Kumar Behera <169166165+Swadhin-0434@users.noreply.github.com>
2024-05-16 18:31:26 +0530

    new file added

---
+++ JS/CLASS-1/01_variable.js
@@ -0,0 +1,43 @@
+// Declare a variable
+
+let num =10;
+console.log("The output is:", num); // to print the output
+
+let Num=100;
+console.log("The output is:", Num);
+
+// (Space dosn't allow while declaring variable.)
+// let second num = 1000;
+
+
+// let 01num =50; it doesn't allow number in the beginning
+//(snake_case)
+let second_num = 1000; 
+//(camelCase)
+let secondNum = 1000; 
+
+let decimal_num = 15.35;
+console.log(decimal_num);
+
+
+// to check data type use 'typeof' operator
+console.log(typeof decimal_num);
+
+// String: 
+// 1. String is a collection of characters.
+// 2. String is a primitive data type.
+
+
+let name = "Swadhin Kumar Behera"
+console.log("My name is", name); // output: My name is: Swadhin Kumar Behera
+// `` - backtick
+console.log(`My name is: ${name}`);
+console.log(typeof name);
+
+//Fun fact:
+let number = "10"
+console.log(typeof number); //output: string
+
+let String = 10
+console.log(typeof String); //output: number
+
---
+++ JS/CLASS-1/task.js
@@ -0,0 +1,23 @@
+// Task:
+// task1: Declare a number variable and assign a value to it.
+// task2: Declare different variables of numbers and assign different values to perform mathematical operation.
+// eg: addition , subtraction , multiplication , division and modulus.
+
+// Examples:
+// let num1 = 12;
+// let num2 = 13;
+// let addition = num1 + num2;
+// console.log(addition); // 25
+
+
+let num1 = 8;
+let num2 = 4;
+
+console.log("The sum of two number is:",num1+num2);
+console.log("The subtraction of two number is:",num1-num2);
+console.log("The multiplication of two number is:",num1*num2);
+console.log("The division of two number is:",num1/num2);
+console.log("The modulus of two number is:", num1%num2);
+console.log("The exponent of two number is:", num1**num2);
+
+
