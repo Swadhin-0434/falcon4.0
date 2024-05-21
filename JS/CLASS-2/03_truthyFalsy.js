e1d1fd4d40c31e28cc96fe564c5749bb10706fa9
Swadhin Kumar Behera <169166165+Swadhin-0434@users.noreply.github.com>
2024-05-21 16:32:18 +0530

    new file added

--- JS/CLASS-2/03_truthyFalsy.js
+++ JS/CLASS-2/03_truthyFalsy.js
@@ -24,10 +24,82 @@
 //     console.log("false");
 // }
 
-let isChecked = -1;
-if (isChecked) {
-    console.log("true");
-} else{
-    console.log("false");
-}
+// let isChecked = -1;
+// if (isChecked) {
+//     console.log("true");
+// } else{
+//     console.log("false");
+// }
+
+// let isChecked =true;
+// if (isChecked) {
+//     console.log("true");
+// } else{
+//     console.log("false");
+// }
+
+// let isChecked = false;
+// if (isChecked) {
+//     console.log("true");
+// } else{
+//     console.log("false");
+// }
+
+// let isChecked = -0;
+// if (isChecked) {
+//     console.log("true");
+// } else{
+//     console.log("false");
+// } 
+
+// let isChecked = "";
+// if (isChecked) {
+//     console.log("true");
+// } else{
+//     console.log("false");
+// }
+
+// let isChecked = null;
+// if (isChecked) {
+//     console.log("true");
+// } else{
+//     console.log("false");
+// }
+
+// let isChecked = undefined;
+// if (isChecked) {
+//     console.log("true");
+// } else{
+//     console.log("false");
+// }
+
+// let isChecked = NaN;
+// if (isChecked) {
+//     console.log("true");
+// } else{
+//     console.log("false");
+// }
+
+// // let isChecked = 0n;
+// // if (isChecked) {
+// //     console.log("true");
+// // } else{
+// //     console.log("false");
+// // }
+
+// // let isChecked = String("Swadhin");
+// // if (isChecked) {
+// //     console.log("true");
+// // } else{
+// //     console.log("false");
+// // }
+
+// // let isChecked = Object();
+// // if (isChecked) {
+// //     console.log("true");
+// // } else{
+// //     console.log("false");
+// // }
+
+
 
---
+++ JS/CLASS-2/Task.js
@@ -0,0 +1,84 @@
+//todo Task: 1
+// Use all the string methods to demonstrate all the properties with examples.
+
+// Example:
+// let sentence = "He is a good boy"; // 16 char
+
+// console.log(sentence.includes("is"));
+
+//todo Task: 2
+// Declare a sentence and include your name in that and check wheather your name is present or not using `include` method.
+
+// Example:
+// let sentence = Lorem ipsum dolor sit amet pramod consectetur adipisicing elit. Perferendis, nihil.
+
+//Task: 2
+// let bio = "I am SWADHIN KUMAR BEHERA."
+// console.log(bio.includes("SWADHIN"));    //true
+
+//Task: 1
+
+
+// [@@iterator]()
+// let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani";
+// console.log(aboutMe.[@@iterator]());
+
+//anchor()
+// let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani";
+// console.log(aboutMe.anchor()); // true
+
+//at()
+// let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani";
+// console.log(aboutMe.at(21)); // n
+
+//big()
+// let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani";
+// console.log(aboutMe.big()); //<big>Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani</big>
+
+//blink()
+// let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani";
+// console.log(aboutMe.blink()); // <blink>Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani</blink>
+
+//bold()
+// let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani";
+// console.log(aboutMe.bold()); // <b>Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani</b>
+
+//charCodeAt()
+// let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani";
+// console.log(aboutMe.charCodeAt(5)); // 33
+
+//codePointAt()
+// let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani";
+// console.log(aboutMe.codePointAt(1)); //101
+
+//concat()
+// let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani";
+// let aboutYou = " How are you?"
+// console.log(aboutMe.concat('',aboutYou)); //Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani How are you?
+
+//endsWith()
+// let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani"
+// console.log(aboutMe.endsWith("Phulbani"));  // true
+
+//fixed()
+// let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani"
+// console.log(aboutMe.fixed()); //<tt>Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani</tt>
+
+//fontsize()
+// let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani"
+// console.log(aboutMe.fontsize(20)); //<font size="20">Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani</font>
+
+//indexOf()
+// let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani"
+// console.log(aboutMe.indexOf("swadhin")); //15
+
+//isWellFormed()
+let aboutMe = "Hello! This is swadhin kumar behera. Pursuing BCA at govt. auto. college, Phulbani"
+console.log(aboutMe.isWellFormed());
+
+//
+
+
+
+
+
