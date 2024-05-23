b11c1423eb5c100af8cd0bf2216dafd25fb66086
Swadhin Kumar Behera <169166165+Swadhin-0434@users.noreply.github.com>
2024-05-23 18:29:57 +0530

    new file added

--- JS/CLASS-3/02_object.js
+++ JS/CLASS-3/02_object.js
@@ -6,34 +6,63 @@
 
 // Traditional method 
 
-// let student = {
-//     name: "Swadhin",
-//     age : 21,
-//     city:"Bhubaneswar",
-//     marks: [90,98,90],
-//     isMarried: false,
-//     address: {po:"RRL",pin:754031},
-// };
+let student = {
+    name: "Swadhin",
+    age : 21,
+    city:"Bhubaneswar",
+    marks: [90,98,90],
+    isMarried: false,
+    address: {po:"RRL",pin:754031},
+};
 // console.log(student);
 
 //  Constructor :
-let newStudent = new Object();
-newStudent.name = "Swadhin";
-newStudent.age = 21;
-newStudent.city = "Bhubaneswar"
-newStudent.marks = [90,98,90];
-newStudent.isMarried = false;
-newStudent.address = { po:"RRL", pin: 754031};
-console.log(newStudent);
+// let newStudent = new Object();
+// newStudent.name = "Swadhin";
+// newStudent.age = 21;
+// newStudent.city = "Bhubaneswar"
+// newStudent.marks = [90,98,90];
+// newStudent.isMarried = false;
+// newStudent.address = { po:"RRL", pin: 754031};
+// console.log(newStudent);
 
 // Accessing object :
 
 // myarray[0]
-console.log(newStudent.name); // dot notation
-console.log(newStudent["marks"]); // bracket notation
-console.log(newStudent.address);
-console.log(newStudent.city);
+// console.log(newStudent.name); // dot notation
+// console.log(newStudent["marks"]); // bracket notation
+// console.log(newStudent.address);
+// console.log(newStudent.city);
 
-console.log(typeof newStudent);
+// console.log(typeof newStudent);
 
- 
+ // Modifying object :
+//  console.log(student);
+//  student.name = "Banty";
+//  student.age = 90;
+//  student.isMarried = true;
+//  student.address = { po:"Mugagahira", pin : 754312}
+//  student.marks = [80,89,87];
+//  student.city = "Cuttack";
+//  console.log(student);
+
+
+ // Deleting object :
+ delete student.age;
+ console.log(student);
+ student.age = 21;
+ console.log(student);
+
+
+ //Object keys
+
+ let keys = Object.keys(student);
+ console.log(keys);  //it gives the only keys [ "name","city", "marks","isMarried","address"]
+
+ //Object values
+ let value = Object.values(student);
+ console.log(value); // it gives you the values
+
+ //Object entries
+ let entry = Object.entries(student);
+ console.log(entry); // it gives you both keys and values
---
+++ JS/CLASS-4/01_control.js
@@ -0,0 +1,69 @@
+// Conditional statements :
+
+// Conditional statement :
+// Statements that are perform different actions based on different conditions, used to make decision in code.
+
+//Types :
+//1. If Statement :
+//2. Else.. if Statement :
+//3. Else Statement :
+//4. Switch Statement :
+//5. Ternary operator :
+
+// If Statement :
+
+//syntax :
+
+//if (condition) {
+    
+//}
+
+// let age =26; //declare a variable
+// if (age >= 18) {
+//     console.log("You are an adult");
+// }
+
+//Else..if Statement :
+
+//syntax :
+
+// let age =26; //declare a variable
+// if (age >= 18) {
+//     console.log("You are an adult");
+// }else if(age == 18){
+//     console.log("You are 18");
+// }else if (age < 18){
+//     console.log("You are a minor");
+// }else{
+//     console.log("Invalid age");
+// }
+
+//Switch case :
+
+//syntax :
+// let result = 2;
+// switch (result) {
+//     case 0:
+//         console.log("You have lost the Game");
+//         break;
+//     case 1:
+//         console.log("Game draw");
+//         break;
+//     case 2:
+//         console.log("You have won the game");
+//         break;
+
+//     default:
+//         console.log("You are not part of the game");
+//         break;
+// }
+
+
+//Ternary operator :
+//syntax : ?
+
+let pass = 60;
+let result = pass >= 60 ? "pass":"fail";
+// console.log(result);
+// pass >= 60 ? console.log("Pass") : console.log("Fail");
+
