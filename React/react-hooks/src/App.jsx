ff312ccb50ca8ddb3606e6fa90a00b17b12ac4ba
Swadhin Kumar Behera <169166165+Swadhin-0434@users.noreply.github.com>
2024-06-01 18:24:20 +0530

    new project added.

--- React/react-hooks/src/App.jsx
+++ React/react-hooks/src/App.jsx
@@ -1,15 +1,21 @@
 import "./App.css";
-import Counter from "./components/usestate/counter/Counter.jsx";
+import Click from "./components/useeffect/Click";
+import Fetch from "./components/useeffect/Fetch";
+// import Form from "./components/useref/Form";
+// import Show from "./components/usestate/showtext/Show";
+// import Counter from "./components/usestate/counter/Counter";
 
-
-function App(){
+function App() {
   return (
     <div>
-    <h1>Learning React Hooks</h1>
-    <Counter/>
+      <h1>React Hooks</h1>
+      {/* <Counter /> */}
+      {/* <Show /> */}
+      {/* <Form /> */}
+      <Click />
+      <Fetch />
     </div>
   );
 }
 
-
-export default App
+export default App;
